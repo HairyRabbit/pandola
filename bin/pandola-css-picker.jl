ad705e21c2cc895db0f8a135a8906d964611a878
Yufi <yfhj1990@hotmail.com>
2016-01-08 15:07:51 +0800

    Update picker.

--- bin/pandola-css-picker.jl
+++ bin/pandola-css-picker.jl
@@ -1,21 +1,32 @@
 #!/usr/bin/env julia
 
-#Pkg.add("AnsiColor")
+home = homedir()
+namespace = "pandola"
 
-str = open(readall, "index.html")
-    
-re = r"class=\"?\'?([\w-\s]+)\"?\'?"
-#
-function pick_class(rm, acc = [])
-    if rm == nothing
-        return acc;
-    else
-        pick_class(match(re, str, rm.offset + 1), push!(acc, rm.match))
-    end
+find_file(x) = begin
+    (root, dirs, files) = x
+    map(a -> "$root/$a", files)
 end
 
-res = pick_class(match(re, str, 1))
+ff = map(find_file, walkdir("$home/$namespace/bin/center"))
 
+#strArr = reduce(append!, [], ff) |> println
+
+strArr = map(x -> open(readall, x), reduce(append!, [], ff))
+    
+re = r"class=\"?\'?([\w-\s]+)\"?\'?"
+
+pick_class(re, str) = begin
+    next_class(rm, acc = []) = begin
+        if rm == nothing
+            return acc;
+        else
+            next_class(match(re, str, rm.offset + 1), push!(acc, rm.match))
+        end
+    end
+
+    next_class(match(re, str, 1))
+end
 match_class(x) = match(re, x).captures[1]
 split_class(x) = split(x, r"\s")
 find_class(x) = x |> match_class |> split_class
@@ -28,11 +39,15 @@ reduce_class(acc, x) = begin
         else
             print("OK, find -> \"$x\". ")
             print("\e[0;32;49mThunk\e[0m\n")
-            push!(acc, x);
+            push!(acc, x)
         end
     end
     foreach(y -> fill_acc(acc, y), x)
     acc
 end
 
-res1 = reduce(reduce_class, [], map(find_class, res)) |> sort # |> println
+res = pick_class(re, reduce(string, strArr))
+res1 = reduce(reduce_class, [], map(find_class, res)) |> sort #|> println
+println("\nFound $(length(res1)) roles.")
+
+map(x -> println(x), res1)
